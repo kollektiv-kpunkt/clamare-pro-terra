ca6f1b192819ff2c1dd1bbd27e5f22a489c8e0d0
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2023-05-09 22:02:39 +0200

    Add OG Tags

--- web/src/components/Head.svelte
+++ web/src/components/Head.svelte
@@ -1,3 +1,7 @@
+<script>
+	import { _ } from 'svelte-i18n';
+</script>
+
 <svelte:head>
 	<link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png" />
 	<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png" />
@@ -8,4 +12,23 @@
 	<meta name="msapplication-TileColor" content="#da532c" />
 	<meta name="msapplication-config" content="/images/favicon/browserconfig.xml" />
 	<meta name="theme-color" content="#ffffff" />
+
+	<!-- Primary Meta Tags -->
+	<title>{$_('og.title')}</title>
+	<meta name="title" content={$_('og.title')} />
+	<meta name="description" content={$_('og.description')} />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://klima-demo.ch/" />
+	<meta property="og:title" content={$_('og.title')} />
+	<meta property="og:description" content={$_('og.description')} />
+	<meta property="og:image" content="" />
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://klima-demo.ch/" />
+	<meta property="twitter:title" content={$_('og.title')} />
+	<meta property="twitter:description" content={$_('og.description')} />
+	<meta property="twitter:image" content="" />
 </svelte:head>
