17ab69997cb1fec79a837f25201636bfa0641c76
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2023-05-10 19:38:02 +0200

    Add Online DOnation Form

--- web/src/components/Donate.svelte
+++ web/src/components/Donate.svelte
@@ -1,5 +1,38 @@
 <script>
-	import { _, json } from 'svelte-i18n';
+	import { onMount } from 'svelte';
+	import { _, json, locale } from 'svelte-i18n';
+	import Button from './Button.svelte';
+
+	let onlineDonationContainer;
+
+	function openDonationForm() {
+		onlineDonationContainer.animate(
+			[
+				{ maxHeight: '0px' },
+				{ maxHeight: onlineDonationContainer.scrollHeight + 'px', offset: 0.99 },
+				{ maxHeight: 'unset' }
+			],
+			{
+				duration: 500,
+				easing: 'ease-in-out',
+				fill: 'forwards'
+			}
+		);
+	}
+
+	onMount(() => {
+		let script = document.createElement('script');
+		script.src = 'https://tamaro.raisenow.com/klima-3a25/latest/widget.js';
+		document.querySelector('.rnw-widget-container').parentNode.appendChild(script);
+		script.onload = () => {
+			window.rnw.tamaro.runWidget('.rnw-widget-container', {
+				language: $locale,
+				paymentFormPrefill: {
+					stored_campaign_name: 'Klimademo | Manif Climat'
+				}
+			});
+		};
+	});
 </script>
 
 <div class="cpt-donate">
@@ -12,4 +45,8 @@
 			{/each}
 		</p>
 	</div>
+	<div class="cpt-online-donation max-h-0 overflow-hidden" bind:this={onlineDonationContainer}>
+		<div class="rnw-widget-container pt-12" />
+	</div>
+	<Button classes="mt-8" onClick={openDonationForm}>Online Spenden</Button>
 </div>
